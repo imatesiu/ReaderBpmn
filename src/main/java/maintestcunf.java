3aaf8741249481e8de1f15a31a0abf0d6986689f
Giorgio <isiu@MacBook-Pro.local>
2015-06-04 18:32:30 +0200

    add cunf test

--- src/main/java/maintestcunf.java
+++ src/main/java/maintestcunf.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Collection;
 
 import javax.swing.JFileChooser;
@@ -6,8 +7,11 @@ import javax.swing.JFrame;
 
 import models.graphbased.directed.bpmn.BPMNDiagram;
 import models.graphbased.directed.petrinet.Petrinet;
+import models.graphbased.directed.petrinet.PetrinetGraph;
 import models.semantics.petrinet.Marking;
+import models.utils.Pair;
 import petrinet.dot.DotImporting;
+import petrinet.pnml.importing.PnmlImportNet;
 import plugins.bpmn.Bpmn;
 import plugins.bpmn.trasform.BpmnToPetriNet;
 import framework.util.SimplePanel;
@@ -26,32 +30,51 @@ public class maintestcunf {
 
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 
-
-				File file = fc.getSelectedFile();
-				Bpmn bpmn = new Bpmn(file);
+				SimplePanel sp = new SimplePanel();
 				
+				File file = fc.getSelectedFile();
+				Petrinet pn = null;
+				Marking marking = null;
+				if(file.getAbsolutePath().contains(".bpmn")){
+					Bpmn bpmn = new Bpmn(file);
 
-				BPMNDiagram BPMNdiagram = 	bpmn.BpmnextractDiagram().iterator().next();
-				BpmnToPetriNet btpn = new BpmnToPetriNet(BPMNdiagram);
-				Petrinet pn = (Petrinet) btpn.getPetriNet();
-				Marking marking = btpn.getMarking();
+
+					BPMNDiagram BPMNdiagram = 	bpmn.BpmnextractDiagram().iterator().next();
+					sp.view(BPMNdiagram);
+					BpmnToPetriNet btpn = new BpmnToPetriNet(BPMNdiagram);
+					pn = (Petrinet) btpn.getPetriNet();
+					marking = btpn.getMarking();
+
+				}else{
+					if(file.getAbsolutePath().contains(".pnml")){
+						PnmlImportNet ipnml = new PnmlImportNet();
+						 Pair<PetrinetGraph, Marking> resulti = ipnml.importFromStream(new FileInputStream(file), file.getName(), file.length());
+						
+						 pn = (Petrinet) resulti.getFirst();
+						 marking = resulti.getSecond();
+					}
+
+
+				}
+
+
 				String pepfile = file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf("."))+".ll_net";
 				Util.toFile(pepfile,pn.toPEP(marking));
-				
+
 				//./cunf ../../../tmp/testbpmn/test1.pep -f dot  -o  testre.dot
-				
+
 				String pathcunf  = "/Users/isiu/cunf/bin/";
 				String output = pepfile+".unfold.dot";
 				String result = Util.executeCommand(pathcunf+"cunf -f dot -o "+output+"  "+pepfile);
-				
+
 				System.out.println(result);
-				
+
 				DotImporting doti = new DotImporting(new File(output));
-				
+
 				Petrinet pnunfold = doti.importPetriNet();
-				SimplePanel sp = new SimplePanel();
+				sp.view(pn);
 				sp.view(pnunfold);
-				sp.view(BPMNdiagram);
+				
 
 			}
 		} catch (Exception e) {
