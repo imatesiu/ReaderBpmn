aee1e49ecc4c7e39bad40c5be3ddf51e07ae4549
Giorgio <isiu@MacBook-Pro.local>
2015-06-23 21:22:58 +0200

    add test

---
+++ src/main/java/mainCoverGrapGen.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Set;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import models.graphbased.directed.bpmn.BPMNDiagram;
+import models.graphbased.directed.petrinet.Petrinet;
+import models.graphbased.directed.petrinet.PetrinetGraph;
+import models.graphbased.directed.transitionsystem.CoverabilityGraph;
+import models.graphbased.directed.transitionsystem.Transition;
+import models.semantics.petrinet.Marking;
+import models.utils.Pair;
+import petrinet.analysis.CoverabilitySet;
+import petrinet.behavioralanalysis.CGGenerator;
+import petrinet.dot.DotImporting;
+import petrinet.pnml.importing.PnmlImportNet;
+import plugins.bpmn.Bpmn;
+import plugins.bpmn.trasform.BpmnToPetriNet;
+import framework.util.SimplePanel;
+import framework.util.Util;
+
+
+public class mainCoverGrapGen {
+	public static void main(String[] args) {
+		try {
+
+			JFileChooser fc = new JFileChooser();
+			fc.setDialogType(JFileChooser.OPEN_DIALOG);		
+			fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+			int returnVal = fc.showOpenDialog(new JFrame("Load File"));
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+				SimplePanel sp = new SimplePanel();
+				
+				File file = fc.getSelectedFile();
+				Petrinet pn = null;
+				Marking marking = null;
+				if(file.getAbsolutePath().contains(".bpmn")){
+					Bpmn bpmn = new Bpmn(file);
+
+
+					BPMNDiagram BPMNdiagram = 	bpmn.BpmnextractDiagram().iterator().next();
+					sp.view(BPMNdiagram);
+					BpmnToPetriNet btpn = new BpmnToPetriNet(BPMNdiagram);
+					pn = (Petrinet) btpn.getPetriNet();
+					marking = btpn.getMarking();
+
+					
+
+				}else{
+					if(file.getAbsolutePath().contains(".pnml")){
+						PnmlImportNet ipnml = new PnmlImportNet();
+						 Pair<PetrinetGraph, Marking> resulti = ipnml.importFromStream(new FileInputStream(file), file.getName(), file.length());
+						
+						 pn = (Petrinet) resulti.getFirst();
+						 marking = resulti.getSecond();
+						 
+						
+					}
+
+
+				}
+
+				 sp.view(pn);
+				CGGenerator cg = new CGGenerator();
+				Object[] result = cg.petriNetToCoverabilityGraph(pn,marking);
+				CoverabilityGraph covGraph = (CoverabilityGraph) result[0];
+				CoverabilitySet covSet = (CoverabilitySet) result[1];
+				System.out.println(result);
+
+				
+				
+				sp.view(covGraph);
+				Set<Transition> v = covGraph.getEdges();
+				System.out.println(v);
+
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+
+	}
+}
